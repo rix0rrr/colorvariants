621edf815a53dd92768d82725221bee79c70cc39
Rico Huijbers <rix0rrr@gmail.com>
2013-10-25 11:08:05 +0200

    Add LZMA compression to stateblob

--- colorvariants.js
+++ colorvariants.js
@@ -9,14 +9,47 @@ function AppModel() {
     self.stateblob = ko.observable('');
     self.renderer  = new RenderDelphi();
 
-    self.render = ko.computed(function() {
-        var blob =  btoa(JSON.stringify(self.colorGrid().toJSON()));
-        return self.renderer.render(self.colorGrid(), blob);
+    var jsonifiedGrid = ko.computed(function() {
+        return JSON.stringify(self.colorGrid().toJSON());
     });
 
+    /**
+     * Convert array to string, for interfacing with the LZMA library
+     */
+    var arrayToStr = function(arr) {
+        var binary = '';
+        var bytes  = new Uint8Array(arr);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return binary;
+    }
+
+    var strToArray = function(str) {
+        var bytes = [];
+        var len = str.length;
+        for (var i = 0; i < len; i++) {
+            bytes.push(str.charCodeAt(i));
+        }
+        return bytes;
+    }
+
+    jsonifiedGrid.subscribe(function(jsonString) {
+        LZMA.compress(jsonString, 1, function(compressed) {
+            var blob = btoa(arrayToStr(compressed));
+            var rendered = self.renderer.render(self.colorGrid(), blob);
+            $('#rendered').text(rendered);
+        });
+    });
+
+
     self.loadStateBlob = function() {
-        var str = JSON.parse(atob(self.stateblob()));
-        self.colorGrid(ColorGrid.fromJSON(str));
+        var compressed = atob(self.stateblob().trim());
+        LZMA.decompress(strToArray(compressed), function(json) {
+            var str = JSON.parse(json);
+            self.colorGrid(ColorGrid.fromJSON(str));
+        });
     }
 
     self.colorGrid().initializeWithSamples();
